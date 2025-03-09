e93b1f548b39e01a81c193bf2cfc28cb1cefbe03
frozenchicken00 <achi5183@gmail.com>
2025-03-08 20:10:14 -1000

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,14 @@
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
 export default function Footer({ darkMode }) {
     return (
         <footer className={`w-full z-10 p-4 ${darkMode ? "bg-gray-800 text-white" : "bg-white text-black"} text-center shadow-lg border-t border-gray-200`}>
-            <a href="https://www.linkedin.com/in/hoyoung-kim-946992281/" target="_blank" rel="noopener noreferrer" className="mx-2">LinkedIn</a>
-            <a href="https://github.com/frozenchicken00" target="_blank" rel="noopener noreferrer" className="mx-2">GitHub</a>
+            <a href="https://www.linkedin.com/in/hoyoung-kim-946992281/" target="_blank" rel="noopener noreferrer" className="mx-2 inline-flex items-center">
+                <FaLinkedin className="mr-1" />
+            </a>
+            <a href="https://github.com/frozenchicken00" target="_blank" rel="noopener noreferrer" className="mx-2 inline-flex items-center">
+                <FaGithub className="mr-1" />
+            </a>
             <p>&copy; Hoyoung Kim 2024</p>
         </footer>
     );
